b66f01b3efda1516d90f906806bdd3bb05e6377b
Ervan Ade Wijaya <89782667+ervanade@users.noreply.github.com>
2026-01-26 16:55:33 +0700

    fix layout

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -3,24 +3,52 @@ import Header from '@/components/header/Header';
 import { Toaster } from '@/components/ui/toaster';
 import { NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
-
+import parse from 'html-react-parser';
+const HTMLDecoderEncoder = require("html-encoder-decoder");
 const locales = ['en', 'id'];
+
+async function getData() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_KEY}/api/v1/web-settings`, {
+    // cache: 'no-store',
+    next: { revalidate: 3600 * 24 },
+    method: 'GET',
+    headers: {
+      'X-Api-Key': process.env.NEXT_PUBLIC_APP_X_API_KEY,
+    },
+  })
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
 
 export default async function LocaleLayout({ children, params }) {
   const { locale } = await params ?? "en";
+  const { settings, navigation } = await getData()
+  const headScript = settings?.before_close_head || ``;
+    const bodyScript = settings?.before_close_body || ``;
 
   if (!locales.includes(locale)) {
     notFound();
   }
 
   return (
- 
-        <NextIntlClientProvider locale={locale}>
-          <Header />
+ <html>
+  <head>
+        {parse(HTMLDecoderEncoder.decode(headScript))}
+      </head>
+      <body>
+      <NextIntlClientProvider locale={locale}>
+          <Header navigation={navigation || []}/>
           {children}
           <Footer />
           <Toaster />
         </NextIntlClientProvider>
+      </body>
+ </html>
+     
    
   );
 }
